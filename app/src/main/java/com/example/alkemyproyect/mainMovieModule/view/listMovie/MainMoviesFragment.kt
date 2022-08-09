c4f3537e6ec8d63cd9cdc8f553e7324231f3ed04
Nicolas <nicolasfuentesg06@gmail.com>
2022-08-09 19:33:22 -0300

    Dejamos el codigo un poco mas limpio

--- app/src/main/java/com/example/alkemyproyect/mainMovieModule/view/listMovie/MainMoviesFragment.kt
+++ app/src/main/java/com/example/alkemyproyect/mainMovieModule/view/listMovie/MainMoviesFragment.kt
@@ -16,14 +16,13 @@ import com.example.alkemyproyect.databinding.FragmentMainMoviesBinding
 import com.example.alkemyproyect.mainMovieModule.view.adapter.MovieAdapter
 import com.example.alkemyproyect.mainMovieModule.viewModels.listMovieViewModels.MovieViewModel
 import com.example.alkemyproyect.mainMovieModule.model.Movie
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 
 class MainMoviesFragment : Fragment(), OnClickListener {
 
     private var _binding: FragmentMainMoviesBinding? = null
     private val binding get() = _binding!!
-
-    private var bandera = true
 
     val viewModel by viewModels<MovieViewModel>()
 
@@ -36,8 +35,10 @@ class MainMoviesFragment : Fragment(), OnClickListener {
         _binding = FragmentMainMoviesBinding.inflate(inflater, container, false)
 
        setupViewModel()
+
         return binding.root
     }
+
 
     private fun setupViewModel() {
 
@@ -45,11 +46,16 @@ class MainMoviesFragment : Fragment(), OnClickListener {
         viewModel.movieList.observe(viewLifecycleOwner, Observer {movie ->
             initMovie(movie)
         })
-        viewModel.error.observe(viewLifecycleOwner, Observer {movie ->
-            Log.i("valor", movie)
+        viewModel.error.observe(viewLifecycleOwner, Observer {error ->
+
+            MaterialAlertDialogBuilder(requireContext()).setTitle(error)
+                .setView(layoutInflater.inflate(R.layout.dialog_alert,null))
+                .setCancelable(false).setPositiveButton("Salir... ",{ _ , i ->
+                    onDestroy()
+            }).show()
         })
-        viewModel.isloading.observe(viewLifecycleOwner, Observer {movie ->
-            if (movie) binding.progressBar.visibility = View.VISIBLE
+        viewModel.isloading.observe(viewLifecycleOwner, Observer {success ->
+            if (success) binding.progressBar.visibility = View.VISIBLE
             else binding.progressBar.visibility = View.GONE
 
         })
@@ -63,80 +69,6 @@ class MainMoviesFragment : Fragment(), OnClickListener {
         binding.recyclerView.adapter = MovieAdapter(movie, this)
         binding.recyclerView.addItemDecoration(decoration)
     }
-/*
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-        connecting()
-    }*/
-
-  /*  private fun connecting(){
-        if (isConnected(requireContext())){
-            bandera = false
-            loadMovies()
-        }
-        else {
-            MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.title)
-                .setView(layoutInflater.inflate(R.layout.dialog_alert, null))
-                .setCancelable(false)
-                .setPositiveButton("reintentar...") { _ , i ->
-                    if (isConnected(requireContext())) {
-                        loadMovies()
-                    } else {
-                        connecting()
-                    }
-
-                    Toast.makeText(requireContext(), "hollaa", Toast.LENGTH_LONG).show()
-                }
-                .show()
-        }
-
-
-    }*/
-    /*private fun isConnected(requireContext: Context): Boolean {
-        val cm = requireContext.getSystemService(Context.CONNECTIVITY_SERVICE)  as ConnectivityManager
-        val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
-        val isConnected: Boolean = activeNetwork?.isConnectedOrConnecting == true
-        return isConnected
-    }
-
-    private fun getRetrofit(): Retrofit {
-        return Retrofit.Builder()
-            .baseUrl(Constants.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }*/
-
-   /* private fun loadMovies(){
-
-        CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit()
-                .create(APIResponse::class.java)
-                .getMyMovies(Constants.API_PATH + Constants.API_POPULAR)
-
-            val movie: MovieResponse? = call.body()
-
-
-            withContext(Dispatchers.Main) {
-                val movies = movie?.movie ?: emptyList()
-
-                if (call.isSuccessful){
-                    binding.progressBar.visibility = View.GONE
-                    initMovie(movies)
-                }else{
-                    MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.title)
-                        .setView(layoutInflater.inflate(R.layout.dialog_alert, null)).setCancelable(false)
-                        .setPositiveButton("reintentar...") { _, i ->
-                            onDestroy()
-                        }
-                        .show()
-                    //show error
-                }
-            }
-
-        }
-
-    }*/
-
 
 
 
@@ -148,4 +80,8 @@ class MainMoviesFragment : Fragment(), OnClickListener {
     }
 
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
--- app/src/main/java/com/example/alkemyproyect/mainMovieModule/viewModels/listMovieViewModels/MovieViewModel.kt
+++ app/src/main/java/com/example/alkemyproyect/mainMovieModule/viewModels/listMovieViewModels/MovieViewModel.kt
@@ -23,22 +23,22 @@ class MovieViewModel: ViewModel() {
 
         viewModelScope.launch {
             isloading.postValue(true)
-            //var issuccess: String? = null
+
             try {
-                // throws TimeoutCancellationException
-               // withTimeout(100) {
-                    //NetworkResponse.Success(apiCall.invoke())
+
+
                       var call = RetrofitHelper.getRetrofit()
                         .create(APIResponse::class.java)
                         .getMyMovies(Constants.API_PATH + Constants.API_POPULAR)
 
 
-                //}
+
 
                 val movie: MovieResponse? = call.body()
                 val movies = movie?.movie ?: emptyList()
                 if (call.isSuccessful) {
                     movieList.postValue(movies)
+
                 } else {
 
                 }
@@ -46,29 +46,19 @@ class MovieViewModel: ViewModel() {
                 throwable.printStackTrace()
                 when (throwable) {
                     is TimeoutCancellationException -> {
-                        val code = 408 // timeout error code
                         error.postValue("se acabo el tiempo")
-                        //NetworkResponse.GenericError(code, NETWORK_ERROR_TIMEOUT)
+
                     }
                     is IOException -> {
 
                         error.postValue("Error de red")
-                        //NetworkResponse.NetworkError
                     }
                     is HttpException -> {
-                        val code = throwable.code()
                         error.postValue("ERrores de servidor")
-                        // NetworkResponse.GenericError(
-                        /*    code,
-                                    errorResponse*/
-                        //   )
                     }
                     else -> {
-                        //error = ":("
-                        // NetworkResponse.GenericError(
-                        //  null,
-                        //  NETWORK_ERROR_UNKNOWN
-                        // )
+                        error.postValue(":(")
+
                     }
                 }
             }
@@ -77,12 +67,6 @@ class MovieViewModel: ViewModel() {
         }
     }
 
-    /* MaterialAlertDialogBuilder(requireContext()).setTitle(R.string.title)
-                                .setView(layoutInflater.inflate(R.layout.dialog_alert, null)).setCancelable(false)
-                                .setPositiveButton("reintentar...") { _, i ->
-                                    onDestroy()
-                                }
-                                .show()
-                            //show error */
+
 }
 
